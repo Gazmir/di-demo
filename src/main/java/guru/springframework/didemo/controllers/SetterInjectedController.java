930c8f4828609f94633caff3ab118f7418d99183
gazmiri <gazmiribrahimi@hotmail.com>
2018-10-24 13:08:12 +0200

    DI without @Primary

--- src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
@@ -2,18 +2,21 @@ package guru.springframework.didemo.controllers;
 
 
 import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
-
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
 
-    public void setGreetingService(GreetingService greetingService) {
+    @Autowired
+    public void setGreetingService(@Qualifier("getterGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    String sayHello() {
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
-
 }
