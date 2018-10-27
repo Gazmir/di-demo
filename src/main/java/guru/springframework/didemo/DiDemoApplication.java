661c8b0420c60862ad6bee26c22b3bbce1748e21
gazmir <gazmiribrahimi@hotmail.com>
2018-10-27 16:19:20 +0200

    @Primary and @Profile

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.didemo;
 
+import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.MyController;
+import guru.springframework.didemo.controllers.PropertyInjectedController;
+import guru.springframework.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +17,11 @@ public class DiDemoApplication {
 
 		MyController Controller = (MyController) ctx.getBean("myController");
 
-		Controller.hello();
+		//Controller.hello();
+
+		System.out.println(ctx.getBean(MyController.class).hello());
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
 }
--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -1,12 +1,19 @@
 package guru.springframework.didemo.controllers;
 
+import guru.springframework.didemo.services.GreetingService;
 import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
-    public GreetingServiceImpl greetingService;
+    @Autowired
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingServiceImpl;
 
-    String sayHello() {
-        return greetingService.sayGreeting();
+    public String sayHello() {
+        return greetingServiceImpl.sayGreeting();
     }
 }
