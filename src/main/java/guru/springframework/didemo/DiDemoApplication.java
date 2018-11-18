0a9a2c97de088a3a6af6721c45af54a9a75d9915
gazmir <gazmiribrahimi@hotmail.com>
2018-11-18 23:13:28 +0100

    application.properties file

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -4,24 +4,36 @@ import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.MyController;
 import guru.springframework.didemo.controllers.PropertyInjectedController;
 import guru.springframework.didemo.controllers.SetterInjectedController;
+import guru.springframework.didemo.examplebeans.FakeDataSource;
+import guru.springframework.didemo.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-		MyController Controller = (MyController) ctx.getBean("myController");
+        MyController Controller = (MyController) ctx.getBean("myController");
 
-		//Controller.hello();
+        //Controller.hello();
 
-		System.out.println(ctx.getBean(MyController.class).hello());
+		/*System.out.println(ctx.getBean(MyController.class).hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
-	}
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());*/
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
+        System.out.println(fakeDataSource.getUrl());
+        System.out.println(fakeDataSource.getPassword());
+
+        FakeJmsBroker fakeJmsBroker = ctx.getBean(FakeJmsBroker.class);
+        System.out.println(fakeJmsBroker.getUrl());
+    }
 }
